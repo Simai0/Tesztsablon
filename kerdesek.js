652ecf300800ee35922f43f0978128071da27ccf
Simai0 <70635823+Simai0@users.noreply.github.com>
2021-06-01 23:49:39 +0200

    Add files via upload

---
+++ kerdesek.js
@@ -0,0 +1,22 @@
+kerdesek = [
+    {
+        kerdes:"kerdes1",
+        valaszok : ["valasz1","valasz2","valasz3","valasz4"],
+        jovalasz: "valasz2"
+    },
+    {
+        kerdes:"kerdes2",
+        valaszok : ["valasz1","valasz2","valasz3","valasz4"],
+        jovalasz: "valasz4"
+    },
+    {
+        kerdes:"kerdes3",
+        valaszok : ["valasz1","valasz2","valasz3","valasz4"],
+        jovalasz: "valasz1"
+    },
+    {
+        kerdes:"kerdes4",
+        valaszok : ["valasz1","valasz2","valasz3","valasz4"],
+        jovalasz: "valasz3"
+    }
+]
